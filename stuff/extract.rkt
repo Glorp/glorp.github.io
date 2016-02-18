84c6c05bc749d66c68c306a482df9db4e2b5e461
Glorp <jonas.winje@gmail.com>
2016-02-18 18:33:37 +0100

    newlining

--- stuff/extract.rkt
+++ stuff/extract.rkt
@@ -11,7 +11,7 @@
             (define-values (dir filename _) (split-path (hash-ref meta 'file)))
             (define newfile (build-path dir (~a (path-replace-suffix filename "") "-extract.rkt")))
             `(,newfile
-              ,(apply ~a (add-between (apply append (map piece->rkt pieces)) "\n\n")))])]))
+              ,(apply ~a (add-between (apply append (map piece->rkt pieces)) "\n")))])]))
      
 
 (define (piece->rkt piece)
@@ -24,20 +24,20 @@
     
     [`(top ,h ,str)
      (if (hash-ref h '#:extract #t)
-         `(,str)
+         `(,(~a str "\n"))
          '())]
     
     [`(eval ,h ,str ,_)
      (if (hash-ref h '#:extract #t)
-         `(,str)
+         `(,(~a str "\n"))
          '())]
     
       [`(hidden ,h ,str)
        (if (hash-ref h '#:extract #f)
-           `(,str)
+           `(,(~a str "\n"))
            '())]
     
     [`(noex ,h ,str)
      (if (hash-ref h '#:extract #t)
-         `(,str)
+         `(,(~a str "\n"))
          '())]))
